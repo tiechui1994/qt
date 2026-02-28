04623f580cc32bd442937dc680b0170abc9d56a2
quinn <2904951429@qq.com>
2026-02-28 01:43:03 +0000

    qt build

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QPushButton button("Hello Windows from Codespaces!");
+    button.resize(300, 100);
+    button.show();
+    return app.exec();
+}
